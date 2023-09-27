e7579e71f7a991da1a9433289fb1d249c44d6da1
kuma0523 <136021879+kuma0523@users.noreply.github.com>
2023-09-27 16:40:35 +0000

    途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,44 @@ class ApplicationController < ActionController::Base
 
   # before_action :authenticate_customers, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(customers)
-    items_path
+  
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+        admin_items_path
+    else
+        items_path
+    end
   end
 
-  def after_sign_out_path_for(customers)
-    new_customer_session_path
+  def
+    after_sign_out_path_for(resource_or_scope)
+      if resource_or_scope == :public
+          new_customer_session_path
+      elsif resource_or_scope == :admin
+        new_admin_session_path
+      else
+        root_path
+      end
   end
 
+  
 
-  def after_sign_in_path_for(admin)
-    admin_items_path
-  end
+  # def after_sign_in_path_for(resource)
+  #   items_path
+  # end
 
-  def after_sign_out_path_for(admin)
-    new_admin_session_path
-  end
+  # def after_sign_out_path_for(resource)
+  #   new_customer_session_path
+  # end
+
+
+  # def after_sign_in_path_for(admin)
+  #   admin_items_path
+  # end
+
+  # def after_sign_out_path_for(admin)
+  #   new_admin_session_path
+  # end
 
 
   protected
