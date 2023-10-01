573dad6610c55abc36e03a68b1a2a75f23b249b7
kuma0523 <136021879+kuma0523@users.noreply.github.com>
2023-10-01 10:08:40 +0000

    中断

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,40 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
   end
+
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+
+    if @cart_item.save
+      redirect_to cart_items_path
+    else
+      @cart_items = CartItem.all
+      render :index
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items
+    else
+      @cart_items = CartItem.all
+      render :index
+    end
+  end
+  
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    book.destroy
+    redirect_to cart_items_path
+  end
+end
+
+private
+
+def cart_item_params
+  params.require(:cart_item).permit(:customer_id, :item_id, :amount)
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,5 +5,9 @@ class Public::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+  
+  
+  
 end
