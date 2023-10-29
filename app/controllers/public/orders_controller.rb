54c3347909ce4a1e8eccd2ce5ef8259d6d3541da
kuma0523 <136021879+kuma0523@users.noreply.github.com>
2023-10-29 16:29:45 +0000

    中断

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,29 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+
+    @cart_items = CartItem.all
+
+    if params[:order][:order_address] == "0"
+
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+
+    elsif params[:order][:order_address] == "1"
+
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
+
+    if @order.save
+      redirect_to complete_orders_path
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
   end
 
   def complete
@@ -14,4 +37,10 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+end
+
+private
+
+def order_params
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
 end
